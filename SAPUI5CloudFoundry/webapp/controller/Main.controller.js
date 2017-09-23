b75a0548f03e341de83a97235149968303654010
P935700 <wouterlemaire120@hotmail.com>
2017-09-23 19:15:19 +0000

    Cloudfoundry with express and mongodb

---
+++ SAPUI5CloudFoundry/webapp/controller/Main.controller.js
@@ -0,0 +1,39 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller,JSONModel) {
+	"use strict";
+
+	return Controller.extend("be.wl.SAPUI5CloudFoundry.controller.Main", {
+		onInit:function(){
+			this.getView().setModel(new JSONModel({item:"",items:[]}));
+			this.getItems();
+		},
+		onAddItem: function() {
+			var oData = this.getView().getModel().getData();
+			
+			var that = this;
+			jQuery.ajax({
+				method: "POST",
+				url: "/item/",
+				data: {
+					item: {item:oData.item}
+				}
+			}).done(function(msg) {
+				that.getItems();
+			});
+		},
+		getItems: function() {
+			var oModel = this.getView().getModel();
+			var oData = oModel.getData();
+			var that = this;
+			jQuery.ajax({
+				method: "GET",
+				url: "/items"
+			}).done(function(data) {
+				oData.items = data;
+				oModel.setData(oData);
+			});
+		}
+	});
+});
